eeca68699cceb46850df1324cd93cad9cb67832b
geethikaa501 <geethikaa501@gmail.com>
2019-06-21 15:40:18 +0530

    first commit

---
+++ src/main/java/com/example/DemoSpg/DemoSpgApplication.java
@@ -0,0 +1,13 @@
+package com.example.DemoSpg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpgApplication.class, args);
+	}
+
+}
